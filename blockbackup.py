79df6009d8c197868035d971b96a523b7817e412
Iuri Rezende Souza <lrezende@usc.edu>
2016-04-13 17:12:13 -0700

    Loop starts

--- blockbackup.py
+++ blockbackup.py
@@ -2,8 +2,13 @@
 
 def aread():
 	with open('block', 'rb') as i:
+		count = 0
 		b1 = i.read(512)
-		print(b1)
+		while(b1):
+			print(count)
+			print(b1)
+			count += 1
+			b1 = i.read(512)
 
 if __name__ == '__main__':
 	print('Hello')
